a9441d62f8026b8f28b20f834ef391c389e7b6e7
Pablo Monti <Pablo_Monti@hotmail.com.ar>
2023-03-20 15:29:52 -0300

    Get purchases generico

---
+++ API/Authenticator/Controllers/PurchasesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices.JavaScript;
+
+namespace Authenticator.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PurchasesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetPurchases()
+        {
+            return Ok(Enumerable.Range(0, 20).Select(x => new
+            {
+                id = Guid.NewGuid(),
+                productDescription = "Product " + x.ToString(),
+                dateOperation = DateTime.Today,
+                state = "delivered"
+            }).ToList());
+        }
+    }
+}
